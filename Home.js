f2b8df1ef22a198a9846de9c527dca1c773c4279
Den <issarajames@gmail.com>
2024-11-01 14:39:44 +0700

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,27 @@
+function silver(){
+
+}
+
+function fiber(){
+
+}
+
+function wooden(){
+
+}
+
+function stone(){
+
+}
+
+function textile(){
+
+}
+
+function ceramic(){
+    
+}
+
+function sil_pro{
+    
+}
